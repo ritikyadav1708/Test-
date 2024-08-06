6f5a6f9dd3d192a03f4216db31c3925bc096ee6a
ritikyadav1708 <152800142+ritikyadav1708@users.noreply.github.com>
2024-08-06 13:13:51 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,2 @@
+// https://api.metalpriceapi.com/v1/latest
+  ?api_key=af03b1c6e75c288e2e8845a6f5fae474
---
+++ index.js
@@ -0,0 +1,13 @@
+for(i=0;i<5;i++){
+    console.log(" ")
+ for(j=0;i+1;j++){
+    console.log("*")
+    break;
+    
+}
+
+
+}
+
+let a=
+
